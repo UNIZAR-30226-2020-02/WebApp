3bf5dc7fa61c63b07827e2c6c39d36bcdcacebf3
a760099 <760099@unizar.es>
2020-03-23 12:48:51 +0100

    Pagina de inicio de sesion sin funcionalidades

--- playstack/src/app/pages/login/login.page.ts
+++ playstack/src/app/pages/login/login.page.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
+
+
 export class LoginPage implements OnInit {
+  @ViewChild('passwordEyeRegister', {static: false}) passwordEye: ElementRef;
+
+  passwordTypeInput = 'password';
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  togglePasswordMode() {
+    //cambiar tipo input
+    this.passwordTypeInput = this.passwordTypeInput === 'text' ? 'password' : 'text';
+    //obtener el input
+    const nativeEl = this.passwordEye.nativeElement.querySelector('input');
+    //obtener el indice de la posición del texto actual en el input
+    const inputSelection = nativeEl.selectionStart;
+    //ejecuto el focus al input
+    nativeEl.focus();
+    //espero un milisegundo y actualizo la posición del indice del texto
+    setTimeout(() => {
+        nativeEl.setSelectionRange(inputSelection, inputSelection);
+    }, 1);
   }
 
 }
