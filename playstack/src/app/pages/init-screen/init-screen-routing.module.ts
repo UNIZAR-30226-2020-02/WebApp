94afe0aaa6d3164b1e1574ed92782f5c264d261e
a760099 <760099@unizar.es>
2020-03-29 17:21:02 +0200

    Pagina registro y enrutado desde pagina principal

--- playstack/src/app/pages/init-screen/init-screen-routing.module.ts
+++ playstack/src/app/pages/init-screen/init-screen-routing.module.ts
@@ -6,7 +6,32 @@ import { InitScreenPage } from './init-screen.page';
 const routes: Routes = [
   {
     path: '',
-    component: InitScreenPage
+    component: InitScreenPage,
+    children: [
+      {
+        path: 'login',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('../login/login.module').then(m => m.LoginPageModule)
+          }
+        ]
+      },
+      {
+        path: 'register',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('../register/register.module').then(m => m.RegisterPageModule)
+          }
+        ]
+      },
+    ]
+  },
+  {
+    path: '',
+    redirectTo: 'init-screen',
+    pathMatch: 'full'
   }
 ];
 
