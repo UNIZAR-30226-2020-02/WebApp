0dae7727a54b39dc508ed09df115a4c9ad85c77d
a760099 <760099@unizar.es>
2020-05-10 19:03:55 +0200

    Peticion de creacion de lista

--- playstack/src/app/pages/crear/modal/modal.page.ts
+++ playstack/src/app/pages/crear/modal/modal.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-modal',
@@ -7,13 +8,24 @@ import { ModalController } from '@ionic/angular';
   styleUrls: ['./modal.page.scss'],
 })
 export class ModalPage implements OnInit {
+  nombreLista: string;
 
-  constructor(private modalCtrl: ModalController) { }
+  constructor(private modalCtrl: ModalController, private http: HttpClient) { }
 
   ngOnInit() {
   }
 
   dismiss() : void{
     this.modalCtrl.dismiss();
+  }
+
+  async crearLista(){
+    const httpOptions={
+      headers : new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    };
+    let postParams = {"NombreUsuario": "Rodolfo", "NombrePlaylist": this.nombreLista};
+    let response = await this.http.post("https://playstack.azurewebsites.net/user/login", postParams, httpOptions)
   }
 }
