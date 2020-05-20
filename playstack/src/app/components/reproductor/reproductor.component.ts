16839f6a668fb1b24eb3e45ff001395c53ca480f
a760099 <760099@unizar.es>
2020-05-20 17:22:44 +0200

    Solucionado merge

--- playstack/src/app/components/reproductor/reproductor.component.ts
+++ playstack/src/app/components/reproductor/reproductor.component.ts
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
 import { Router, ActivatedRoute, NavigationExtras } from '@angular/router';
 import { ColaReproduccionPageRoutingModule } from './../../pages/cola-reproduccion/cola-reproduccion-routing.module';
 import { ColaReproduccionPage } from './../../pages/cola-reproduccion/cola-reproduccion.page';
-=======
->>>>>>> aab93a2c4076c33ddad9709ba003db0a4ba7c848
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { ReproductorService } from '../../services/reproductor/reproductor.service';
 
@@ -20,11 +17,7 @@ export class ReproductorComponent implements OnInit {
   @ViewChild('range', {static: false}) range: IonRange;
 
 
-<<<<<<< HEAD
   constructor(public rs: ReproductorService, public router: Router, private activatedRoute: ActivatedRoute) { }
-=======
-  constructor(public rs: ReproductorService) { }
->>>>>>> aab93a2c4076c33ddad9709ba003db0a4ba7c848
   
   seek()
   {
@@ -38,7 +31,6 @@ export class ReproductorComponent implements OnInit {
 
   abrirColaReproduccion() {
     console.log("Abrir cola");
-<<<<<<< HEAD
     // Abrir pantalla de visualización de playlist pasando a la página el objeto que contiene la playlist
     
     let navigationExtras: NavigationExtras = {
@@ -46,8 +38,6 @@ export class ReproductorComponent implements OnInit {
     };
     console.log(this.activatedRoute);
     this.router.navigate(['../../cola-reproduccion']), navigationExtras;
-=======
->>>>>>> aab93a2c4076c33ddad9709ba003db0a4ba7c848
   }
   ngOnInit() {}
 
