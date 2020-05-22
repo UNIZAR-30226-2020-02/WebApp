57a7d875bf40bcae3ce7f519e870bea20b45d4e9
a760099 <760099@unizar.es>
2020-05-22 14:18:10 +0200

    HTML de artista

--- playstack/src/app/pages/artista/artista.page.ts
+++ playstack/src/app/pages/artista/artista.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ReproductorService } from 'src/app/services/reproductor/reproductor.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-artista',
@@ -6,10 +9,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./artista.page.scss'],
 })
 export class ArtistaPage implements OnInit {
-
-  constructor() { }
+  canciones: Observable<any>;
+  albumes: Observable<any>;
+  constructor(private route: ActivatedRoute, private router: Router, private nombreArtista: string,
+      private rs: ReproductorService) { }
 
   ngOnInit() {
+    this.canciones = this.rs.getCancionesByArtista(this.nombreArtista);
+    this.albumes = this.rs.getArtistaAlbums(this.nombreArtista);
   }
 
 }
