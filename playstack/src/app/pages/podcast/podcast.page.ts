e3c9a051f98a602565190cf3390e50a9bfa9937a
fernando <ferpb1999@gmail.com>
2020-05-24 18:23:24 +0200

    Arreglando podcasts

--- playstack/src/app/pages/podcast/podcast.page.ts
+++ playstack/src/app/pages/podcast/podcast.page.ts
@@ -1,4 +1,3 @@
-import { IonicModule } from '@ionic/angular';
 import { ContenidoService } from 'src/app/services/contenido/contenido.service';
 import { Component, OnInit, Input } from '@angular/core';
 import { ReproductorService, Podcast } from 'src/app/services/reproductor/reproductor.service';
@@ -12,34 +11,32 @@ import { Observable } from 'rxjs';
   styleUrls: ['./podcast.page.scss'],
 })
 export class PodcastPage implements OnInit {
-
+  podcast: any; // Falta por hacer una clase podcast
   @Input()
-  nombrePodcast: string;
-
-  podcast: Promise<any>;
-  infoPodcast: any;
+  listaCapitulos: Observable<any>;
 
   showSpinner: boolean = false;
-  showError: boolean = false;
-  mensajeError: string = "No hay episodios";
+  showError : boolean = true;
 
   constructor(private rs: ReproductorService, private http: HttpClient, private cs: ContenidoService,
     private route: ActivatedRoute, private router: Router) { 
       this.route.queryParams.subscribe(() => {
         if (this.router.getCurrentNavigation().extras.state) {
-          this.nombrePodcast = this.router.getCurrentNavigation().extras.state.podcast;
+          this.podcast = this.router.getCurrentNavigation().extras.state.podcast;
         }
       });
     }
 
-  async ngOnInit() {
-    this.podcast = this.cs.getInformacionPodcast(this.nombrePodcast).toPromise();
+  ngOnInit() {
+    this.listaCapitulos = this.cs.getEpisodios(this.podcast.key);
+    this.cargarCapitulos();
+  }
 
-    waits this.podcast.then(
-      informacion => {
+  cargarCapitulos() {
+    this.listaCapitulos.subscribe(
+      capitulos => {
         this.showSpinner = false;
-        this.infoPodcast = informacion;
-        if (this.infoPodcast.capitulos == []) {
+        if (this.isEmpty(capitulos)) {
           this.showError = true;
         }
       },
@@ -54,19 +51,10 @@ export class PodcastPage implements OnInit {
     return Object.keys(obj).length === 0;
   }
 
-  /*
-  addChapter(cancion: any, indice: number) {
-    if (this.podcast.tracks[indice] === undefined) {
-      this.playlist.tracks[indice] = this.c.constructTrack(cancion);
-      console.log(this.playlist.tracks);
-    }
-  }
-
   playChapter(capitulo: any) {
     // Establece la lista de reproducción del reproductor como las
     // canciones que hay después de la que se pone (incluida).
     this.rs.setListaAudio(this.podcast.capitulos);
     this.rs.start(capitulo);
   }
-  */
 }
